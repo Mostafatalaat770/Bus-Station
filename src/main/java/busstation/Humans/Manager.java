0b8cbf36ce90da19206605004f6c77b59eb86ee2
Mostafa Talaat <mostafatalaat770@gmail.com>
2019-04-05 21:12:01 +0200

    Initial manager class.

---
+++ src/main/java/busstation/Humans/Manager.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package busstation.Humans;
+
+
+public class Manager extends Employees {
+    
+    
+    public Manager(String name, int age, String address) {
+        super(name, age, address);
+    }
+
+}
