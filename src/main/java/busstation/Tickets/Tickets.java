3d5549ff781a58cfac2ea9365567b2c732116144
Mostafa Talaat <mostafatalaat770@gmail.com>
2019-04-06 14:08:18 +0200

    Initial version.

---
+++ src/main/java/busstation/Tickets/Tickets.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package busstation.Tickets;
+
+/**
+ *
+ * @author Mostafa Talaat
+ */
+public abstract class Tickets {
+    
+}
