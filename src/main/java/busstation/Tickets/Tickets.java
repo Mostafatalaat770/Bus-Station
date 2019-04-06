6a03be174c98400e3af1e2a18349343154a9b593
Mostafa Talaat <mostafatalaat770@gmail.com>
2019-04-06 21:47:48 +0200

    Initial Tickets class.

--- src/main/java/busstation/Tickets/Tickets.java
+++ src/main/java/busstation/Tickets/Tickets.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package busstation.Tickets;
 
 /**
@@ -11,5 +10,63 @@ package busstation.Tickets;
  * @author Mostafa Talaat
  */
 public abstract class Tickets {
-    
+
+    private double price;
+    private boolean refundable;
+    private boolean priority;
+    private double discountPrecent;
+
+    public double calculate(boolean VIP) {
+        if (VIP) {
+            return (price * discountPrecent);
+        }
+        return price;
+    }
+    public boolean payment(double price){
+        if(price >= this.price)
+        {
+            this.price-=price;
+            return true;
+        }
+        return false;
+    }
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isRefundable() {
+        return refundable;
+    }
+
+    public void setRefundable(boolean refundable) {
+        this.refundable = refundable;
+    }
+
+    public boolean isPriority() {
+        return priority;
+    }
+
+    public void setPriority(boolean priority) {
+        this.priority = priority;
+    }
+
+    public double getDiscountPrecent() {
+        return discountPrecent;
+    }
+
+    public void setDiscountPrecent(double discountPrecent) {
+        this.discountPrecent = discountPrecent;
+    }
+
+    public Tickets(double price, boolean refundable, boolean priority, double discountPrecent) {
+        this.price = price;
+        this.refundable = refundable;
+        this.priority = priority;
+        this.discountPrecent = discountPrecent;
+    }
+
 }
