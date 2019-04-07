a32957bd117175075b8ade055e167524ddc4ddf6
Mostafa Talaat <mostafatalaat770@gmail.com>
2019-04-08 00:12:15 +0200

    Created Files structure interface.

---
+++ src/main/java/busstation/Files/FileManagement.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package busstation.Files;
+
+import java.io.File;
+
+/**
+ *
+ * @author DELL
+ */
+public interface FileManagement {
+
+    public boolean readFile();
+
+    public boolean writeFile();
+
+}
