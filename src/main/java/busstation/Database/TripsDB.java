03d6ffd1b175ebfad2194d67ccacbbd2a6f52135
Mostafa Talaat <mostafatalaat770@gmail.com>
2019-04-11 00:07:03 +0200

    Updated methods parameters to match 9e11712.

--- src/main/java/busstation/Database/TripsDB.java
+++ src/main/java/busstation/Database/TripsDB.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package busstation.Database;
 
 import busstation.Trips.ExternalTrip;
@@ -18,22 +17,22 @@ public class TripsDB {
 
     ArrayList<InternalTrip> internalTripsDB = new ArrayList<>();
     ArrayList<ExternalTrip> externalTripsDB = new ArrayList<>();
-    
-    public boolean createInternalTrip(String name, String startPos, String endPos, String distance, double duration, int stopType){
-            internalTripsDB.add(new InternalTrip(name, startPos, endPos, distance, duration, stopType));
-            return true;
-        }
-        public boolean createExternalTrip(String name, String startPos, String endPos, String distance, double duration, int stopType){
-            externalTripsDB.add(new ExternalTrip(name, startPos, endPos, distance, duration, stopType));
-            return true;
-        }
-        public InternalTrip getTrip(){
-        return internalTripsDB.get(0);
+
+    public boolean createInternalTrip(String name, String startPos, String endPos, double starTime, double endTime, double price, double discountPrecent, int stopType) {
+        internalTripsDB.add(new InternalTrip(name, startPos, endPos, starTime, endTime, price, discountPrecent, stopType));
+        return true;
     }
-    public ArrayList<InternalTrip> getInternalTrips(){
+
+    public boolean createExternalTrip(String name, String startPos, String endPos, double starTime, double endTime, double price, double discountPrecent, int stopType) {
+        externalTripsDB.add(new ExternalTrip(name, startPos, endPos, starTime, endTime, price, discountPrecent, stopType));
+        return true;
+    }
+
+    public ArrayList<InternalTrip> getInternalTrips() {
         return internalTripsDB;
     }
-    public ArrayList<ExternalTrip> getExternalTrips(){
+
+    public ArrayList<ExternalTrip> getExternalTrips() {
         return externalTripsDB;
     }
 }
